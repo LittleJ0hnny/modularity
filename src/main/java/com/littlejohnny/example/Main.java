629a2df611db88223695acaca01a087ffd3729f7
OKravtsiv <kravtsivostap@gmail.com>
2019-01-03 17:28:47 +0200

    Edited project structure

---
+++ src/main/java/com/littlejohnny/example/Main.java
@@ -0,0 +1,9 @@
+package com.littlejohnny.example;
+
+import com.littlejohnny.folderlistener.HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module modularity {
+    requires folderlistener;
+}
